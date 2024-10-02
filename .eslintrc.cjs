80235c5608b9767d4f527ea4b49776e401dd4cc1
hcw0915 <love233031@gmail.com>
2024-10-02 13:44:37 +0800

    feat: add simple-sort

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  root: true,
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -6,8 +7,27 @@ module.exports = {
     '@electron-toolkit/eslint-config-ts/recommended',
     '@electron-toolkit/eslint-config-prettier'
   ],
+  plugins: ['simple-import-sort'],
   rules: {
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // 1. React and third-party packages (node_modules)
+          ['^react', '^\\w'],
+          // 2. Internal components and modules (aliases like @components)
+          ['^@', '^[^.]'],
+          // 3. Static files: Images, SVGs, and styles (css, scss, etc.)
+          ['^.+\\.(png|jpg|svg|css|scss)$']
+        ]
+      }
+    ],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-unused-vars': 'off'
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 'latest'
   }
 }
--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,7 +1,8 @@
-import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 import macrosPlugin from 'vite-plugin-babel-macros'
+
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {
